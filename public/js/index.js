64e52da24872b3f1b1e1f0b363198d5248c99489
Rodel Ruiz <Rodel Ruiz>
2016-12-26 12:48:02 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function ()  {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Rodel',
-    text: 'Yup, that works for me'
-  });
 });
 
 socket.on('disconnect', function () {
